2a99bed4bf81519d06232c87649c819a7adcbf6d
Mathieu Amiot <m.amiot@otak-arts.com>
2014-02-18 18:38:45 +0100

    Initial commit

---
+++ OKBlurredView/OKBlurredView.h
@@ -0,0 +1,16 @@
+//
+//  OKBlurredView.h
+//  OKBlurredView
+//
+//  Created by Mathieu Amiot on 18/02/2014.
+//  Copyright (c) 2014 Mathieu Amiot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface OKBlurredView : UIView
+
+@property (nonatomic, setter=setRefreshRate:) float refreshRate;
+
+@end
---
+++ OKBlurredView/OKBlurredView.m
@@ -0,0 +1,58 @@
+//
+//  OKBlurredView.m
+//  OKBlurredView
+//
+//  Created by Mathieu Amiot on 18/02/2014.
+//  Copyright (c) 2014 Mathieu Amiot. All rights reserved.
+//
+
+#import "OKBlurredView.h"
+#import "GPUImageiOSBlurFilter.h"
+
+@implementation OKBlurredView
+{
+    NSTimer *_fps;
+    UIView *_blurView;
+    GPUImageiOSBlurFilter *_blurFilter;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self)
+    {
+        _blurFilter = [GPUImageiOSBlurFilter new];
+
+        _blurView = [[UIView alloc] initWithFrame:self.bounds];
+        [self addSubview:_blurView];
+
+        self.refreshRate = 30.0f;
+    }
+    return self;
+}
+
+- (void)setRefreshRate:(float)refreshRate {
+    [_fps invalidate];
+    _fps = nil;
+    _fps = [NSTimer timerWithTimeInterval:(1.0f / self.refreshRate)
+                                   target:self
+                                 selector:@selector(_updateBackgroundBlur)
+                                 userInfo:nil
+                                  repeats:YES];
+}
+
+- (void)_updateBackgroundBlur
+{
+    if (!self.superview)
+        return;
+
+    CGRect snapBounds = self.bounds;
+    UIGraphicsBeginImageContext(snapBounds.size);
+    [self.superview drawViewHierarchyInRect:snapBounds afterScreenUpdates:YES];
+    UIImage *bg = [_blurFilter imageByFilteringImage:UIGraphicsGetImageFromCurrentImageContext()];
+    UIGraphicsEndImageContext();
+    _blurView.backgroundColor = [UIColor colorWithPatternImage:bg];
+    [_blurView setNeedsDisplay];
+}
+
+@end
